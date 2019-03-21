032c04ab108a5fb3007cf114cc5a5f96b4bc51b7
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-03-21 11:35:57 +0200

    added test suite (enzyme and redux-mock-store)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import {Provider} from "react-redux";
+
+let store;
+
+describe('<App />', () => {
+  beforeEach(() => {
+    const mockStore = configureStore();
+    store = mockStore({
+      rotating: true
+    });
+  });
+
+  it('Should render the component with mock store', () => {
+    const wrapper = shallow(<Provider store={store}>
+      <App />
+    </Provider>);
+    expect(wrapper).not.toBe(null);
+    expect(wrapper.contains(<App />)).toEqual(true);
+  });
 });
