5b7a3809f641e411c14859f35de7bfe91b750914
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-05-13 08:45:31 +0300

    added VideoList.test.js file to test api api calls

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -5,20 +5,7 @@ import thunk from "redux-thunk";
 
 import App from '../App';
 import {Provider} from "react-redux";
-import {apiGetVideosSuccessCall} from "../actions/apiAction";
-import getAction from './utils/getAction';
-
 const mockStore = configureStore([thunk]);
-
-
-const videos =  [
-        { "id": 616, "name": "Vierailuluento: Avaruudesta avaruustieteitä.", "length": 34342342, "owner": "xyz_kuvaaja" },
-        { "id": 612, "name": "Mansikoiden lisääntyminen umpimetsässä.", "length": 34342342, "owner": "xyz_kuvaaja" },
-        { "id": 614, "name": "Voittokulku ja kuinka se taklataan. Projektioita projektimaailmasta.", "length": 34342342, "owner": "xyz_kuvaaja" },
-        { "id": 615, "name": "Autonomisen yhteiskunnan mahdollisuudet ja uhat maapallolla.", "length": 34342342, "owner": "xzz_kuvaaja" },
-        { "id": 617, "name": "Atomien välisten yhteyksien hahmottaminen Cernin ulkopuolella.", "length": 34342342, "owner": "xyx_kuvaaja" }
-    ]
-
 
 describe('<App />', () => {
 
@@ -27,7 +14,7 @@ describe('<App />', () => {
 
     const initialState =  {
         //rotate : {rotating: true}, 
-        api: {error: "", videos: videos}
+        vr: {error: "", videos:[]}
     }
 
     beforeEach(() => {
@@ -43,14 +30,4 @@ describe('<App />', () => {
         expect(wrapper.contains(<App />)).toEqual(true);
     });
 
-
-    // it('Should pass actions updated values ', async() => {
-    //     expect(store.getActions().length).toBe(0);
-    //     store.dispatch(apiGetVideosSuccessCall(videos));
-    //     expect(await getAction(store, "api")).not.toBe(null);
-    //     expect(await getAction(store, "api")).toEqual({
-    //         "type": "SUCCESS_API_GET_VIDEOS",
-    //         "payload": videos
-    //     });
-    // });
 });
