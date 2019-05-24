26d3766d7b76f1ba2b5edad9ac6cab35354c4c08
Toni Räsänen <toni.rasanen@helsinki.fi>
2019-05-24 13:33:45 +0300

    redirect and route to shibbo login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import './App.css';
 import VideoList from './components/VideoList';
+import { BrowserRouter as Router,Route, Redirect } from 'react-router-dom';
 
 
 const App = (props) => {
-    return (
-        <div className="App">
 
-            {/* { <p>Fetching the data from the backend :</p>}
+
+  const LATAAMO_DEV = 'https://lataamo-dev.it.helsinki.fi/Shibboleth.sso/Login';
+
+  return (
+    <div className="App">
+
+      {/* { <p>Fetching the data from the backend :</p>}
             {props.api.error && <p>{props.api.error}</p>} */}
 
-            <div>
-                <h2> Videos</h2>
-                <p>REACT_APP_LATAAMO_PROXY_SERVER: {process.env.REACT_APP_LATAAMO_PROXY_SERVER}</p>
-                <VideoList />
-            </div>
-        </div>
-    );
-}
+      <div>
+        <h2> Videos</h2>
+        <p>REACT_APP_LATAAMO_PROXY_SERVER: {process.env.REACT_APP_LATAAMO_PROXY_SERVER}</p>
+        <VideoList />
+      </div>
+
+      <Router>
+        <h1>Unitube lataamo</h1>
+        <Redirect to="/login" />
+        <Route path="/login" render={() => window.location.href = LATAAMO_DEV} />
+      </Router>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
-    error : state.vr.error
+  error : state.vr.error
 });
 
 export default connect(mapStateToProps, null)(App);
