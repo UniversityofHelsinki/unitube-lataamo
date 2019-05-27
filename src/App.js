cc87ed69cc97134c98d5b036970e717447b23131
hpalva <henriikka.palva@helsinki.fi>
2019-05-27 12:10:21 +0300

    LATAAMO-109 initial layout structure

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { connect } from "react-redux";
 import './App.css';
 import VideoList from './components/VideoList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './stylesheets/main.sass';
 
 
 const App = (props) => {
     return (
         <div className="App">
-
-            {/* { <p>Fetching the data from the backend :</p>}
-            {props.api.error && <p>{props.api.error}</p>} */}
-
-            <div>
+            <Header />
+            <div className="content-wrapper">
                 <h2> Videos</h2>
                 <p>REACT_APP_LATAAMO_PROXY_SERVER: {process.env.REACT_APP_LATAAMO_PROXY_SERVER}</p>
                 <VideoList />
             </div>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <>
+        <div className="hynavbar">HY Header</div>
+        <div className="navbar">Lataamo header</div>
+        </>
+    );
+};
+
+export default Header;
