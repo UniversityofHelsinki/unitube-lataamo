5c44d7786bb18dc0e70cf78a45a405476b61e847
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-05-29 08:06:26 +0300

    useEffect to see if timestamp has changed since last 401 error, if so fire login page again

--- src/components/LoginRedirect.js
+++ src/components/LoginRedirect.js
@@ -1,26 +1,24 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 
 const LoginRedirect = (props) => {
-
-
-  useEffect(() => {
-    if(props.redirect401) {
-      window.location.assign(props.loginUrl);
-    }
-  }, [props.redirect401]);
-  return null;
+    useEffect(() => {
+        if(props.redirect401) {
+            window.location.assign(props.loginUrl);
+        }
+    }, [props.redirect401]);
+    return null;
 };
 
 LoginRedirect.propTypes = {
-  loginUrl: PropTypes.string.isRequired
+    loginUrl: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
-  redirect401 : state.sr.redirect401,
-  apiError : state.sr.apiError
+    redirect401 : state.sr.redirect401,
+    apiError : state.sr.apiError
 });
 
 export default connect(mapStateToProps, null)(LoginRedirect);
