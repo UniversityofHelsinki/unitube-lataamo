dd7a68bb3a6421dae482bfd05f42a2ee95723a67
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-06-07 13:46:09 +0300

    lataamo-122

---
+++ src/actions/seriesAction.js
@@ -0,0 +1,38 @@
+// asynchronous action creator
+
+export const fetchSeries = () => {
+
+    // server from .env variable
+    const VIDEO_SERVER_API = process.env.REACT_APP_LATAAMO_PROXY_SERVER;
+    const PATH = '/api/userSeries';
+
+    return async (dispatch) => {
+        try {
+            let response = await fetch(`${VIDEO_SERVER_API}${PATH}`);
+            if(response.status === 200) {
+                let responseJSON = await response.json();
+                dispatch(apiGetSeriesSuccessCall(responseJSON));
+            } else {
+                dispatch(api401FailureCall(new Date()));
+            }
+        } catch(err) {
+            dispatch(apiFailureCall('Unable to fetch data'));
+        }
+    };
+};
+
+export const apiGetSeriesSuccessCall = data => ({
+    type: 'SUCCESS_API_GET_SERIES',
+    payload: data
+});
+
+export const api401FailureCall = failureTime => ({
+    type: 'STATUS_401_API_CALL',
+    payload : failureTime
+});
+
+
+export const apiFailureCall = msg => ({
+    type: 'FAILURE_API_CALL',
+    payload: msg
+});
--- src/translations.js
+++ src/translations.js
@@ -7,7 +7,10 @@ const translationsObject = {
         video_title: 'Video title',
         video_duration: 'Duration of video',
         video_id : 'Video identifier',
-        redirecting: 'Redirecting to login page...'
+        redirecting: 'Redirecting to login page...',
+        serie_id: 'serie id',
+        serie_title: 'title',
+        serie_contributors: 'contributors'
     },
     fi: {
         lataamo: 'Lataamo',
@@ -17,7 +20,10 @@ const translationsObject = {
         video_title: 'Videon nimi',
         video_duration: 'Videon kesto',
         video_id : 'Videon tunniste',
-        redirecting: 'Ohjataan kirjautumissivulle ...'
+        redirecting: 'Ohjataan kirjautumissivulle ...',
+        serie_id: 'sarjan tunniste',
+        serie_title: 'sarjan otsikko',
+        serie_contributors: 'sarjan omistajat'
     },
     sv: {
         lataamo: 'Loader',
@@ -27,7 +33,10 @@ const translationsObject = {
         video_title: 'Video titeln',
         video_duration: 'Videolängd',
         video_id : 'Video tagg',
-        redirecting: 'Riktas till inloggningssidan ...'
+        redirecting: 'Riktas till inloggningssidan ...',
+        serie_id: 'serie tagg',
+        serie_title: 'serie titeln',
+        serie_contributors: 'serie ägare'
     }
 };
 
