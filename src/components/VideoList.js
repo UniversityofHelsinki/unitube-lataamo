cd6a6413d6ebe7533788c93c0a0653a9b6daecd6
Antti Lankinen <antti.lankinen@helsinki.fi>
2019-08-29 08:13:13 +0300

    Lisää uusi video linkin ympärille div

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { fetchVideo, fetchVideos } from '../actions/videosAction';
-import { fetchEvent } from '../actions/eventsAction';
-import { fetchSeries } from '../actions/seriesAction';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {fetchVideo, fetchVideos} from '../actions/videosAction';
+import {fetchEvent} from '../actions/eventsAction';
+import {fetchSeries} from '../actions/seriesAction';
 import BootstrapTable from 'react-bootstrap-table-next';
-import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import Video from './Video';
 import VideoDetailsForm from './VideoDetailsForm';
@@ -12,11 +12,11 @@ import moment from 'moment';
 import {Translate} from "react-redux-i18n";
 import {Link} from "react-router-dom";
 
-const { SearchBar } = Search;
+const {SearchBar} = Search;
 
 const VideoList = (props) => {
 
-    const translations =  props.i18n.translations[props.i18n.locale];
+    const translations = props.i18n.translations[props.i18n.locale];
 
     const translate = (key) => {
         return translations ? translations[key] : '';
@@ -47,7 +47,7 @@ const VideoList = (props) => {
             <div>
                 {
                     row.visibility.map((acl, index) =>
-                        <p key={index}> { acl } </p>
+                        <p key={index}> {acl} </p>
                     )
                 }
             </div>
@@ -66,9 +66,9 @@ const VideoList = (props) => {
         dataField: 'created',
         text: translate('created'),
         type: 'date',
-        sort:true,
+        sort: true,
         formatter: dateFormatter
-    },{
+    }, {
         dataField: 'title',
         text: translate('video_title'),
         sort: true
@@ -126,37 +126,42 @@ const VideoList = (props) => {
 
     return (
         <div>
-            <Link to="/uploadVideo" className="btn btn-primary" >
-                <Translate value="add_video" />
-            </Link>
+            <div className="margintop">
+                <Link to="/uploadVideo" className="btn btn-primary">
+                    <Translate value="add_video"/>
+                </Link>
+            </div>
             <div className="table-responsive">
                 <ToolkitProvider
                     bootstrap4
                     keyField="identifier"
-                    data={ translatedVideos() }
-                    columns={ columns }
+                    data={translatedVideos()}
+                    columns={columns}
                     search>
                     {
                         props => (
                             <div>
-                                <br />
-                                <SearchBar { ...props.searchProps } placeholder={translate('search')} />
-                                <hr />
-                                <BootstrapTable { ...props.baseProps } selectRow={ selectRow } pagination={ paginationFactory() } defaultSorted={ defaultSorted } noDataIndication="Table is Empty" bordered={ false } rowStyle={ rowStyle }  hover />
+                                <br/>
+                                <SearchBar {...props.searchProps} placeholder={translate('search')}/>
+                                <hr/>
+                                <BootstrapTable {...props.baseProps} selectRow={selectRow}
+                                                pagination={paginationFactory()} defaultSorted={defaultSorted}
+                                                noDataIndication="Table is Empty" bordered={false} rowStyle={rowStyle}
+                                                hover/>
                             </div>
                         )
                     }
                 </ToolkitProvider>
-                <Video />
-                <VideoDetailsForm />
+                <Video/>
+                <VideoDetailsForm/>
             </div>
         </div>
     );
 };
 
 const mapStateToProps = state => ({
-    videos : state.vr.videos,
-    selectedRowId : state.vr.selectedRowId,
+    videos: state.vr.videos,
+    selectedRowId: state.vr.selectedRowId,
     i18n: state.i18n
 });
 
