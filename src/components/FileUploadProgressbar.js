b0e2e24ac642119a93d683c3d6cda4052e1dffed
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-08-29 15:40:16 +0300

    lataamo-162
    - css tweaking
    - showing percent in progressbar

--- src/components/FileUploadProgressbar.js
+++ src/components/FileUploadProgressbar.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const FileUploadProgressbar = (props) => {
-
-    console.log(props.percentage);
-
     return (
-        <div className="progress-bar-container">
+        <div>
             <Filler percentage={props.percentage} />
         </div>
     );
 };
 
 const Filler = (props) => {
-    return <div className="progress-bar" style={{ width: `${props.percentage}%` }} />;
+    const showProgress = () => {
+        return parseInt(props.percentage);
+    };
+
+    return (
+        <div className="progress-bar" style={{ width: `${props.percentage}%` }} >
+            <span>{ showProgress() }% Complete</span>
+        </div>
+    );
 };
 
 const mapStateToProps = state => ({
