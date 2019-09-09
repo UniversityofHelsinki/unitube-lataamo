0a8b67dc39cf7351335a56cf99b666a34e1b091b
hpalva <henriikka.palva@helsinki.fi>
2019-09-09 17:14:29 +0300

    LATAAMO-199 videofile selection reset after success

--- src/components/VideoUploadForm.js
+++ src/components/VideoUploadForm.js
@@ -20,7 +20,6 @@ const VideoUploadForm = (props) => {
     const [selectedButtonDisabled, setButtonDisabled] = useState(false);
 
     useEffect(() => {
-        document.getElementById("chosen_file").value=null;
         props.onFetchSeries();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.fur.updateSuccessMessage, props.fur.updateFailedMessage]);
@@ -56,7 +55,7 @@ const VideoUploadForm = (props) => {
     };
 
     const clearVideoFileSelection = () => {
-        setVideoFile(null);
+        document.getElementById("upload_video_file").reset();
     };
 
     return (
@@ -75,11 +74,11 @@ const VideoUploadForm = (props) => {
                 </Alert>
                 : (<></>)
             }
-            <form encType="multipart/form-data" onSubmit={handleSubmit} className="was-validated">
+            <form id="upload_video_file" encType="multipart/form-data" onSubmit={handleSubmit} className="was-validated">
                 <div className="form-group row">
                     <label htmlFor="title" className="col-sm-2 col-form-label">Video file</label>
                     <div className="col-sm-8">
-                        <input id="chosen_file" onChange={handleFileInputChange} type="file" className="form-control" name="video_file" required/>
+                        <input onChange={handleFileInputChange} type="file" className="form-control" name="video_file" required/>
                     </div>
                     <div className="col-sm-2">
                         <button className="btn btn-primary" data-rh={translate('video_file_info')}>?</button>
