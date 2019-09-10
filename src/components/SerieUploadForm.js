2f139511c32d831fe07c7ce0b270467cf96cb443
hpalva <henriikka.palva@helsinki.fi>
2019-09-10 16:02:26 +0300

    LATAAMO-208 initial series upload page

---
+++ src/components/SerieUploadForm.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Translate } from 'react-redux-i18n';
+
+const SerieUploadForm = (props) => {
+    return(
+        <div>
+        </div>
+    );
+};
+
+export default SerieUploadForm;
