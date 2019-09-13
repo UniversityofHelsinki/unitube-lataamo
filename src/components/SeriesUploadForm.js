09dfbf5ebac05b71d3e5ed586317216e8f1ec832
hpalva <henriikka.palva@helsinki.fi>
2019-09-13 12:09:37 +0300

    LATAAMO-201 initial form

--- src/components/SeriesUploadForm.js
+++ src/components/SeriesUploadForm.js
@@ -5,6 +5,31 @@ import { Translate } from 'react-redux-i18n';
 const SeriesUploadForm = (props) => {
     return(
         <div>
+            <form>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Sarjan nimi</label>
+                    <div className="col-sm-8">
+                        <input type="text" name="seriesTitle" className="form-control" required/>
+                    </div>
+                    <div className="col-sm-2">
+                        <button className="btn btn-primary">?</button>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Kuvaus</label>
+                    <div className="col-sm-8">
+                        <textarea type="text" name="seriesDescription" className="form-control" maxLength="1500" required/>
+                    </div>
+                    <div className="col-sm-2">
+                        <button className="btn btn-primary">?</button>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="col-sm-10 offset-sm-9">
+                        <button type="submit" className="btn btn-primary">Tallenna</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
