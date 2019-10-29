dff4fc3d6c92cdfb300bf5b65f90fde6febb06f1
vmheikki <veli-matti.heikkila@helsinki.fi>
2019-10-29 09:21:50 +0200

    fixed tests

--- src/__test__/VideoList.test.js
+++ src/__test__/VideoList.test.js
@@ -18,7 +18,8 @@ const videos = [
         'duration': 0,
         'creator': 'xyz_kuvaaja',
         'visibility': [],
-        'created': '2019-05-09T07:34:13Z'
+        'created': '2019-05-09T07:34:13Z',
+        'series': 'sarja1'
     },
     {
         'identifier': 'e4ff3ffe-e32a-42f2-8967-20b44cdc20e1',
@@ -26,7 +27,8 @@ const videos = [
         'duration': 33000,
         'creator': 'xyz_kuvaaja',
         'visibility': [],
-        'created': '2019-05-09T07:34:13Z'
+        'created': '2019-05-09T07:34:13Z',
+        'series': 'sarja2'
     },
     {
         'identifier': '4a6dc481-be36-4d3a-a813-bf415374b83d',
@@ -34,7 +36,8 @@ const videos = [
         'duration': 0,
         'creator': 'xyz_kuvaaja',
         'visibility': [],
-        'created': '2019-05-09T07:34:13Z'
+        'created': '2019-05-09T07:34:13Z',
+        'series': 'sarja3'
     },
     {
         'identifier': 'eb68f711-04f9-4b58-b6c8-582a77d7b8a5',
@@ -42,7 +45,8 @@ const videos = [
         'duration': 0,
         'creator': 'xzz_kuvaaja',
         'visibility': [],
-        'created': '2019-05-09T07:34:13Z'
+        'created': '2019-05-09T07:34:13Z',
+        'series': 'sarja1'
     },
     {
         'identifier': 'e269af0d-3c68-457d-90b5-08da5b531152',
@@ -50,7 +54,8 @@ const videos = [
         'duration   ': 25000,
         'creator': 'xyx_kuvaaja',
         'visibility': [],
-        'created': '2019-05-09T07:34:13Z'
+        'created': '2019-05-09T07:34:13Z',
+        'series': 'sarja1'
     }
 ];
 
@@ -64,7 +69,8 @@ const translations = {
         video_duration: 'video duration',
         processing_state: 'Processing state',
         publication_status: 'Publication Status',
-        created: 'Created'
+        created: 'Created',
+        series_title: 'Series'
     },
     fi: {
         video_id: 'videon id',
@@ -75,7 +81,8 @@ const translations = {
         video_duration: 'video duration',
         processing_state: 'Tallennuksen tila',
         publication_status: 'Tallenteen julkisuus',
-        created: 'Luotu'
+        created: 'Luotu',
+        series_title: 'Sarja'
     },
     sv: {
         video_id: 'video id',
@@ -86,7 +93,8 @@ const translations = {
         video_duration: 'video duration',
         processing_state: 'processing_state',
         publication_status: 'publicitet av inspelningen',
-        created: 'Skapad'
+        created: 'Skapad',
+        series_title: 'Serie'
     }
 };
 
