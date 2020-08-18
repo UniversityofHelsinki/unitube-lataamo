de2cf266c168ff1d57de77df42a342508348a326
Ville Träff <ville.traff@helsinki.fi>
2020-08-18 11:38:13 +0300

    Update translations.js

--- src/translations.js
+++ src/translations.js
@@ -167,7 +167,7 @@ const translationsObject = {
         remove_webvtt_successful : 'Subtitle file deleted successfully!',
         remove_webvtt_failed : 'Failed to delete subtitle file!',
         text_track_upload_help_link:'https://helpdesk.it.helsinki.fi/help/11070',
-        text_track_upload_help_link_text: 'Instructions for adding text track file'
+        text_track_upload_help_link_text: 'Instructions for creating caption files on the Helpdesk web site'
     },
     fi: {
         lataamo_title: 'Unitube-lataamo',
@@ -337,7 +337,7 @@ const translationsObject = {
         remove_webvtt_successful : 'Tekstitystiedoston poisto onnistui!',
         remove_webvtt_failed : 'Tekstitystiedoston poisto epäonnistui!',
         text_track_upload_help_link:'https://helpdesk.it.helsinki.fi/help/11070',
-        text_track_upload_help_link_text: 'Ohjeita videon tekstittämiseen'
+        text_track_upload_help_link_text: 'Ohjeita videon tekstittämiseen Helpdesk-sivustolla'
     },
 
     sv: {
@@ -508,7 +508,7 @@ const translationsObject = {
         remove_webvtt_successful : 'Textningsfilen raderades!',
         remove_webvtt_failed : 'Det gick inte att radera textningsfilen!',
         text_track_upload_help_link:'https://helpdesk.it.helsinki.fi/help/11070',
-        text_track_upload_help_link_text: 'Någon hjelp'
+        text_track_upload_help_link_text: 'Instruktioner för textning finns på Helpdesk-webbplatsen'
     }
 };
 
