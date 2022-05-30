6f3d8b85c56cde99bbc4fdf41c494e87fe39e14d
Riku Hänninen <riku.hanninen@helsinki.fi>
2022-05-30 14:54:57 +0300

    LATAAMO-527 - Download-kuvake tuplana -- kursorin siirtäminen latauspainikkeen päälle näyttää nyt tietoa ladattavasta videosta.

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { fetchVideoUrl, downloadVideo } from '../actions/videosAction';
 import { fetchEvent, fetchEvents, deselectEvent, deselectRow, fetchDeletionDate } from '../actions/eventsAction';
 import { fetchSeries, fetchSeriesDropDownList } from '../actions/seriesAction';
-import { Button } from 'react-bootstrap';
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import paginationFactory from 'react-bootstrap-table2-paginator';
@@ -118,13 +118,20 @@ const VideoList = (props) => {
     };
 
     const mediaFormatter = (cell, row) => {
+        const labels = {
+            resolution: (x,y) => `${x}x${y}`,
+            bitrate: (bitrate) => `${Math.round(bitrate/1000)} kbps`,
+            size: (duration, bitrate) => `${Math.max(1, Math.round((duration/1000) * (bitrate/8) / 10**6))} MB`
+        };
         return (
             <div className="form-container">
                 {
                     row.media.map((media, index) =>
                         <form key={index} onSubmit={handleSubmit}>
                             <input type="hidden" name="mediaUrl" value={media} />
-                            <Button name="downloadButton" className="disable-enable-buttons" variant="link" type="submit"><FiDownload></FiDownload></Button>
+                            <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-disabled">{`${labels.resolution(row.publications[media].width, row.publications[media].height)} - ${labels.bitrate(row.publications[media].bitrate)} - ${labels.size(row.publications[media].duration, row.publications[media].bitrate)}`}</Tooltip>}>
+                                <Button name="downloadButton" className="disable-enable-buttons" variant="link" type="submit"><FiDownload></FiDownload></Button>
+                            </OverlayTrigger>
                             <Button name="downloadIndicator" hidden disabled variant="link"><FaSpinner className="icon-spin"></FaSpinner></Button>
                         </form>
                     )
@@ -145,6 +152,23 @@ const VideoList = (props) => {
         }else {
             return translate('event_failed_state');
         }
+    };
+
+    const downloadColumnFormatter = (column, colIndex, components) => {
+        return (
+            <div>
+                <span style={{ paddingRight: '10px', position: 'relative', top: '4px'  }}>{translate('download_video')}</span>
+                <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{translate('download_video_info')}</Tooltip>}>
+                    <span className="d-inline-block">
+                        <Button disabled style={{
+                            pointerEvents: 'none',
+                            paddingTop: '0px',
+                            paddingBottom: '0px'
+                        }}>{translate('info_box_text')}</Button>
+                    </span>
+                </OverlayTrigger>
+            </div>
+        );
     };
 
     const columns = [{
@@ -181,7 +205,7 @@ const VideoList = (props) => {
         sort: true
     }, {
         dataField: 'media',
-        text: translate('download_video'),
+        headerFormatter: downloadColumnFormatter,
         formatter: mediaFormatter,
         //näin saadaan estettyä haitarin avautuminen, kun klikataan download tallenne
         events: {
